d33b72d78c4906298bec186bb0a794b0cacc6e78
GavinKWade01 <94082783+GavinKWade01@users.noreply.github.com>
2021-11-10 16:22:01 -0600

    Add files via upload

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("          .");
+            Console.WriteLine("        ('");
+            Console.WriteLine("        '|");
+            Console.WriteLine("        |'");
+            Console.WriteLine("       [::]");
+            Console.WriteLine("       [::]   _......_");
+            Console.WriteLine("       [::].-'      _.-`.");
+            Console.WriteLine("       [:.'    .-. '-._.-`.");
+            Console.WriteLine("       [/ /\   |  \        `-..");
+            Console.WriteLine("       / / |   `-.'      .-.   `-.");
+            Console.WriteLine("      /  `-'            (   `.    `.");
+            Console.WriteLine("     |           /\      `-._/      \ ");
+            Console.WriteLine("     '    .'\   /  `.           _.-'| ");
+            Console.WriteLine("    /    /  /   \_.-'        _.':;:/");
+            Console.WriteLine("  .'     \_/             _.-':;_.-'");
+            Console.WriteLine(" /   .-.             _.-' \;.-'");
+            Console.WriteLine("/   (   \       _..-'     |");
+            Console.WriteLine("\    `._/  _..-'    .--.  |");
+            Console.WriteLine(" `-.....-'/  _ _  .'    '.|");
+            Console.WriteLine("          | |_|_| |      | \ ");
+            Console.WriteLine("          | |_|_| |      | |");
+            Console.WriteLine("         /  ''''' |     o|  \ ");
+            Console.WriteLine("         |        |      |  |");
+            Console.WriteLine("         `-.._    /__..--'\.'\n");
+
+        }
+    }
+}
---
+++ conditionalBranching/conditionalBranching/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conditionalBranching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first and last name: ");
+
+            string username = Console.ReadLine();
+
+            Console.WriteLine("\nPlease enter your age:");
+
+            int age = Convert.ToInt16(Console.ReadLine());
+        }
+    }
+}
